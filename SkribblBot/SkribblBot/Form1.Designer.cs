9c8b02d49b8d53ad1f053f69f3d39bb88cd83f34
AtleSkaanes <118199811+AtleSkaanes@users.noreply.github.com>
2022-12-31 14:56:34 +0100

    Add project files.

---
+++ SkribblBot/SkribblBot/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace SkribblBot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DrawingProgressBar = new System.Windows.Forms.ProgressBar();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.ConfigureButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.filePathText = new System.Windows.Forms.Label();
+            this.pictureBoxOrigin = new System.Windows.Forms.PictureBox();
+            this.pictureBoxGray = new System.Windows.Forms.PictureBox();
+            this.SizeLabel = new System.Windows.Forms.Label();
+            this.TimerLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOrigin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGray)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DrawingProgressBar
+            // 
+            this.DrawingProgressBar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.DrawingProgressBar.Location = new System.Drawing.Point(112, 87);
+            this.DrawingProgressBar.Name = "DrawingProgressBar";
+            this.DrawingProgressBar.Size = new System.Drawing.Size(211, 23);
+            this.DrawingProgressBar.TabIndex = 3;
+            this.DrawingProgressBar.Click += new System.EventHandler(this.DrawingProgressBar_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.StartButton.Enabled = false;
+            this.StartButton.Location = new System.Drawing.Point(276, 115);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(100, 25);
+            this.StartButton.TabIndex = 2;
+            this.StartButton.Text = "Start (y)";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // ConfigureButton
+            // 
+            this.ConfigureButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ConfigureButton.Location = new System.Drawing.Point(170, 115);
+            this.ConfigureButton.Name = "ConfigureButton";
+            this.ConfigureButton.Size = new System.Drawing.Size(100, 25);
+            this.ConfigureButton.TabIndex = 1;
+            this.ConfigureButton.Text = "Configure (u)";
+            this.ConfigureButton.UseVisualStyleBackColor = true;
+            this.ConfigureButton.Click += new System.EventHandler(this.ConfigureButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ExitButton.Location = new System.Drawing.Point(64, 115);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(100, 25);
+            this.ExitButton.TabIndex = 0;
+            this.ExitButton.Text = "Exit (i)";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // filePathText
+            // 
+            this.filePathText.AutoSize = true;
+            this.filePathText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.filePathText.Location = new System.Drawing.Point(0, 0);
+            this.filePathText.Name = "filePathText";
+            this.filePathText.Size = new System.Drawing.Size(10, 16);
+            this.filePathText.TabIndex = 4;
+            this.filePathText.Text = " ";
+            this.filePathText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBoxOrigin
+            // 
+            this.pictureBoxOrigin.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBoxOrigin.Location = new System.Drawing.Point(112, 25);
+            this.pictureBoxOrigin.Name = "pictureBoxOrigin";
+            this.pictureBoxOrigin.Size = new System.Drawing.Size(100, 50);
+            this.pictureBoxOrigin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxOrigin.TabIndex = 5;
+            this.pictureBoxOrigin.TabStop = false;
+            // 
+            // pictureBoxGray
+            // 
+            this.pictureBoxGray.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pictureBoxGray.Location = new System.Drawing.Point(223, 25);
+            this.pictureBoxGray.Name = "pictureBoxGray";
+            this.pictureBoxGray.Size = new System.Drawing.Size(100, 50);
+            this.pictureBoxGray.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxGray.TabIndex = 6;
+            this.pictureBoxGray.TabStop = false;
+            // 
+            // SizeLabel
+            // 
+            this.SizeLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.SizeLabel.AutoSize = true;
+            this.SizeLabel.Location = new System.Drawing.Point(3, 180);
+            this.SizeLabel.Name = "SizeLabel";
+            this.SizeLabel.Size = new System.Drawing.Size(10, 16);
+            this.SizeLabel.TabIndex = 7;
+            this.SizeLabel.Text = " ";
+            // 
+            // TimerLabel
+            // 
+            this.TimerLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.TimerLabel.AutoSize = true;
+            this.TimerLabel.Location = new System.Drawing.Point(329, 94);
+            this.TimerLabel.Name = "TimerLabel";
+            this.TimerLabel.Size = new System.Drawing.Size(0, 16);
+            this.TimerLabel.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(453, 201);
+            this.Controls.Add(this.TimerLabel);
+            this.Controls.Add(this.SizeLabel);
+            this.Controls.Add(this.pictureBoxGray);
+            this.Controls.Add(this.pictureBoxOrigin);
+            this.Controls.Add(this.filePathText);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.ConfigureButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.DrawingProgressBar);
+            this.Name = "Form1";
+            this.Text = "SkribblBot";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOrigin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGray)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar DrawingProgressBar;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button ConfigureButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Label filePathText;
+        private System.Windows.Forms.PictureBox pictureBoxOrigin;
+        private System.Windows.Forms.PictureBox pictureBoxGray;
+        private System.Windows.Forms.Label SizeLabel;
+        private System.Windows.Forms.Label TimerLabel;
+    }
+}
+
---
+++ SkribblBot/SkribblBot/Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Input;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.AxHost;
+
+namespace SkribblBot
+{
+    public partial class Form1 : Form
+    {
+        // Classes
+        Click click = new Click();
+        GlobalKeyboardHook globalKeyboardHook = new GlobalKeyboardHook();
+
+
+        // Bools
+        bool gotImage = false;
+        bool isConfigured = false;
+        bool isDrawing = false;
+
+        // Configure size
+        bool isConfiguring = false;
+        bool firstPressed = false;
+        bool secondPressed = false;
+        int[] firstPos = new int[2];
+        int[] secondPos = new int[2];
+        Bitmap resizedImg = null;
+
+        // Strings
+        string currentFilePath = string.Empty;
+        string[] accepetedFileTypes = { ".png", ".jpeg", ".jpg" };
+
+        int[] acceptedInput = { 75, 73, 85, 89 };
+                             //  k   i   u   y
+        
+        // Image bitmap
+        Bitmap imgBitmap = null;
+
+        // Image dimensions
+        int originalWidth;
+        int originalHeight;
+        int newWidth;
+        int newHeight;
+
+        // Timer
+        int timerSeconds;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+            this.TopMost = true;
+
+            globalKeyboardHook.KeyboardPressed += OnKeyPressed;
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] filePaths = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+            foreach (string file in filePaths)
+            {
+                if (accepetedFileTypes.Any(element => Path.GetExtension(file) == element))
+                {
+                    filePathText.ForeColor = Color.Black;
+                    currentFilePath = file;
+                    filePathText.Text = currentFilePath;
+                    //imgBitmap = new Bitmap(currentFilePath);
+                    imgBitmap = new Bitmap(System.Drawing.Image.FromFile(currentFilePath));
+                    pictureBoxOrigin.Image = System.Drawing.Image.FromFile(currentFilePath);
+                    GrayScaleImage();
+                }
+                else
+                {
+                    filePathText.ForeColor = Color.Red;
+                    filePathText.Text = Path.GetExtension(file)+" is not a supported file extension";
+                }
+                StartButton.Enabled = (isConfigured && gotImage);
+
+                if (isConfigured) { ChangeImgSize(); }
+
+            }
+        }
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            DrawImage();
+        }
+
+        private void ConfigureButton_Click(object sender, EventArgs e)
+        {
+            ConfigureSize();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            ExitApp(true);
+        }
+
+        private void DrawingProgressBar_Click(object sender, EventArgs e)
+        {
+        }
+
+        void GrayScaleImage()
+        {
+            // Get image dimensions
+            originalWidth = imgBitmap.Width;
+            originalHeight = imgBitmap.Height;
+
+            // color of the pixel
+            Color pixel;
+
+            //Grayscale
+            for(int y = 0; y < originalHeight; y++)
+            {
+                for (int x = 0; x < originalWidth; x++)
+                {
+                    // Get pixel value
+                    pixel = imgBitmap.GetPixel(x, y);
+
+                    // Get ARGB from pixel value
+                    int A = pixel.A;
+                    int R = pixel.R;
+                    int G = pixel.G;
+                    int B = pixel.B;
+
+                    // Get average
+                    int average = (R + G + B) / 3;
+
+                    // Turn Contrast up to max (Only full balck and full white allowed)
+                    if (average >= 127)
+                    {
+                        average = 255;
+                    }
+                    else { average = 0; }
+
+                    // Set new pixel value
+                    imgBitmap.SetPixel(x,y,Color.FromArgb(A,average,average,average));
+                }
+            }
+
+            // Load grayscale Image in picturebox
+            pictureBoxGray.Image = imgBitmap;
+            gotImage = true;
+        }
+
+        public void ConfigureSize()
+        {
+            ConfigureButton.Text = "Cancel (u)";
+
+            isConfiguring = true;
+            firstPressed = false;
+            secondPressed = false;         
+        }
+
+        void FirstConfigPressed()
+        {
+            firstPressed = true;
+            firstPos[0] = Cursor.Position.X;
+            firstPos[1] = Cursor.Position.Y;
+        }
+
+        void SecondConfigPressed()
+        {
+            secondPressed = true;
+            secondPos[0] = Cursor.Position.X;
+            secondPos[1] = Cursor.Position.Y;
+            ConfigureButton.Text = "Configure (u)";
+            if (gotImage) { ChangeImgSize(); }
+            isConfigured = true;
+            isConfiguring = false;
+            StartButton.Enabled = (isConfigured && gotImage);
+        }
+
+        // src: https://stackoverflow.com/a/10445101
+        void ChangeImgSize()
+        {
+            int width = Math.Abs(firstPos[0] - secondPos[0]);
+            int height = Math.Abs(firstPos[1] - secondPos[1]);
+
+            if (width <= 2 || height <= 2)
+            {
+                MessageBox.Show("No pixels selected");
+                return;
+            }
+
+            int newWidth;
+            int newHeight;
+
+            //float scale = Math.Min(width / imgBitmap.Width, height / imgBitmap.Height);
+            if (imgBitmap.Height >= imgBitmap.Width)
+            {
+                newWidth = (int)(height * imgBitmap.Width / imgBitmap.Height);
+                newHeight = (height);
+            }
+            else
+            {
+                newWidth = (width);
+                newHeight = (int)(width * imgBitmap.Height / imgBitmap.Width);
+            }
+
+            resizedImg = new Bitmap((int)width, (int)height);
+            var graph = Graphics.FromImage(resizedImg);
+            var brush = new SolidBrush(Color.White);
+
+            graph.FillRectangle(brush, new RectangleF(0, 0, width, height));
+            graph.DrawImage(imgBitmap, ((int)width - newWidth) / 2, ((int)height - newHeight) / 2, newWidth, newHeight);
+
+            resizedImg.Save("Resized.png");
+
+            pictureBoxGray.Image = resizedImg;
+            SizeLabel.Text = "new Size: "+width+"x"+height+"  original size: "+originalWidth+"x"+originalHeight;
+
+        }
+
+        void DrawImage()
+        {
+            timerSeconds = 0;
+
+            TimeSpan startTime = DateTime.Now.TimeOfDay;
+            TimeSpan runTime;
+
+            if (!isConfigured || !gotImage) return;
+
+            DrawingProgressBar.Maximum = resizedImg.Width * resizedImg.Height;
+
+            StartButton.Text = "Stop (y)";
+            StartButton.ForeColor = Color.Red;
+            isDrawing = true;
+
+            int startX = Math.Min(firstPos[0], secondPos[0]);
+            int startY = Math.Min(firstPos[1], secondPos[1]);
+
+            for (int y = 0; y < resizedImg.Height; y++)
+            {
+                for (int x = 0; x < resizedImg.Width; x++)
+                {
+                    MoveCursor(x+startX, y+startY);
+                    if (resizedImg.GetPixel(x, y).R == 0)
+                    {
+                        click.leftClick();
+                        Thread.Sleep(1);
+                    }
+                    DrawingProgressBar.Value = y * resizedImg.Width + x;
+                    
+                    //TimerLabel.Text = runTime.ToString();
+
+                    if (!isDrawing) { break; }
+                }
+                if (!isDrawing) { break; }
+            }
+            isDrawing = false;
+            if (!isDrawing)
+            {
+                StartButton.Text = "Start (y)";
+                StartButton.ForeColor = Color.Black;
+            }
+            DrawingProgressBar.Value = 0;
+            runTime = DateTime.Now.TimeOfDay - startTime;
+            TimerLabel.Text = runTime.ToString("mm\\:ss");
+        }
+
+        void MoveCursor(int x, int y)
+        {
+            this.Cursor = new Cursor(Cursor.Current.Handle);
+            Cursor.Position = new Point(x,y);
+        }
+
+        void OnKeyPressed(object sender, GlobalKeyboardHookEventArgs e)
+        {
+            //if (acceptedInput.All(element => e.KeyboardData.VirtualCode != element))
+                //return;
+
+            // y
+            if (e.KeyboardData.VirtualCode == 89 && e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyUp)
+            {
+                if (!isDrawing)
+                {
+                    DrawImage();
+                }
+                else { isDrawing = false; }
+                Thread.Sleep(50);
+            }
+
+            // u
+            if (e.KeyboardData.VirtualCode == 85 && e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyUp)
+            {
+                ConfigureSize();
+                Thread.Sleep(50);
+            }
+
+            // i
+            if (e.KeyboardData.VirtualCode == 73 && e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyUp)
+            {
+                ExitApp(true);
+                Thread.Sleep(50);
+            }
+
+            if (isConfiguring)
+            {
+                if (!secondPressed && firstPressed)
+                {
+                    if (e.KeyboardData.VirtualCode == 75 && e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyUp)
+                    {
+                        SecondConfigPressed();
+                        Thread.Sleep(50);
+                    }
+                }
+
+                if (!firstPressed)
+                {
+                    if (e.KeyboardData.VirtualCode == 75 && e.KeyboardState == GlobalKeyboardHook.KeyboardState.KeyUp)
+                    {
+                        FirstConfigPressed();
+                        Thread.Sleep(50);
+                    }
+                }
+            }
+        }
+
+        void ExitApp(bool showDialog)
+        {
+            if (showDialog)
+            {
+                DialogResult exitDialog = MessageBox.Show("Do you want to exit?", "Exit?", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (exitDialog == DialogResult.Yes)
+                {
+                    this.Close();
+                }
+            }
+            else{ this.Close(); }
+        }
+    }
+}
